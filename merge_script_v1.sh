dca4a35faff4039e10956bf81317e585bf270a1c
Luis Altuve <laltuve@ext.sebbusiness.com>
2024-12-12 09:11:35 -0500

    script to update http-server package from main branch

--- merge_script_v1.sh
+++ merge_script_v1.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-branches=("2-for-each" "2-step" "4-map-reloded" "5-filter" "6-reduce" "7-reduce-reloaded")
+branches=("1-init" "2-for-each" "2-step" "4-map-reloded" "5-filter" "6-reduce" "7-reduce-reloaded")
 for branch in "${branches[@]}"; do
     git checkout $branch
     git merge main
